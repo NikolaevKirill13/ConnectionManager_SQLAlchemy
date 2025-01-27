d05f7844232bbe1f174a3c918f2446bcc18bb2be
st <storm_ne@inbox.ru>
2025-01-27 18:20:29 +0300

    bugfix

--- connection_manager/manager.py
+++ connection_manager/manager.py
@@ -1,26 +1,21 @@
+from typing import Callable
 from contextlib import asynccontextmanager
 from functools import wraps
-from typing import Callable
-from sqlalchemy.ext.asyncio import async_sessionmaker
 
 
 class ConnectionManager:
-
     """
     Менеджер подключений к базе данных. Создает сессии для одной или нескольких баз данных.
-    Работает с помощью декоратора attach_session
+    Работает с помощью декоратора attach_session.
     """
-    def __init__(self, **kwargs: async_sessionmaker) -> None:
+    def __init__(self, **kwargs) -> None:
         """
         :param kwargs: async_sessionmaker
         """
-
-        # Инициализация соединения или других параметров
-        # Создаем сессии для каждого переданного аргумента
         for name, session_factory in kwargs.items():
-            self.create_get_session_method(session_factory, name)
+            self._create_async_session_method(session_factory, name)
 
-    def create_get_session_method(self, session_factory, name):
+    def _create_async_session_method(self, session_factory, name: str):
         """
         Создает метод для получения асинхронной сессии.
         """
@@ -31,27 +26,24 @@ class ConnectionManager:
                 try:
                     yield session
                 except Exception as e:
-                    await session.rollback()
+                    await session.rollback()  # Откат транзакции при ошибке
                     raise e
                 finally:
                     await session.close()
-
         setattr(self, f"get_async_{name}", get_async_session)
 
     def attach_session(self, *names: str):
         """
-        Декоратор для привязки одной или нескольких сессий к функции.
+        Декоратор для привязки одной или нескольких асинхронных сессий к функции.
         """
         def wrapper(func: Callable):
             @wraps(func)
             async def inner(*args, **kwargs):
-                # Получаем соответствующие асинхронные сессии
                 sessions = {}
                 for name in names:
                     session_name = f"get_async_{name}"
                     async with getattr(self, session_name)() as session:
                         sessions[name] = session
-                        # Объединяем сессии в kwargs
                 kwargs.update(sessions)
                 return await func(*args, **kwargs)
 
